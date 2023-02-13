afcae3f9e097abff9550dc280355cd440eafe479
Manca.19321 <Manca.19321@LABI1-6PC06>
2023-02-13 10:49:59 +0100

    modifiche finali

--- Ecommerce/Form1.Designer.cs
+++ Ecommerce/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
             this.button4 = new System.Windows.Forms.Button();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.listView1 = new System.Windows.Forms.ListView();
             this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Nome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -50,10 +49,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(213, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(160, 9);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 28);
+            this.button1.Size = new System.Drawing.Size(67, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,10 +59,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(309, 11);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(232, 9);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 28);
+            this.button2.Size = new System.Drawing.Size(69, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
@@ -72,10 +69,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(768, 11);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(576, 9);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(188, 28);
+            this.button3.Size = new System.Drawing.Size(141, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Svuota";
             this.button3.UseVisualStyleBackColor = true;
@@ -83,20 +79,18 @@
             // 
             // domainUpDown1
             // 
-            this.domainUpDown1.Location = new System.Drawing.Point(16, 15);
-            this.domainUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.domainUpDown1.Location = new System.Drawing.Point(12, 12);
             this.domainUpDown1.Name = "domainUpDown1";
             this.domainUpDown1.ReadOnly = true;
-            this.domainUpDown1.Size = new System.Drawing.Size(189, 22);
+            this.domainUpDown1.Size = new System.Drawing.Size(142, 20);
             this.domainUpDown1.TabIndex = 6;
             this.domainUpDown1.Text = "domainUpDown1";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(572, 11);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(429, 9);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(188, 28);
+            this.button4.Size = new System.Drawing.Size(141, 23);
             this.button4.TabIndex = 7;
             this.button4.Text = "Visualizza";
             this.button4.UseVisualStyleBackColor = true;
@@ -113,10 +107,10 @@
             this.Varie,
             this.Descrizione});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(16, 46);
-            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Location = new System.Drawing.Point(12, 37);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(940, 362);
+            this.listView1.Size = new System.Drawing.Size(706, 295);
             this.listView1.TabIndex = 8;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -157,46 +151,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 420);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(13, 341);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Totale:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(571, 423);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(428, 344);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(73, 420);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(55, 341);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(421, 423);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(316, 344);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.Size = new System.Drawing.Size(106, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Totale senza sconto:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(969, 450);
+            this.ClientSize = new System.Drawing.Size(727, 366);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -208,7 +198,6 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -222,7 +211,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DomainUpDown domainUpDown1;
         private System.Windows.Forms.Button button4;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader ID;
         private System.Windows.Forms.ColumnHeader Nome;
